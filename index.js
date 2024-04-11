accf745c8bf764436658e0c5f2b8bcd28f4d4171
lenny4469 <162060373+lenny4469@users.noreply.github.com>
2024-04-11 10:30:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,3 +21,49 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+function createEmployeeRecords(arr) {
+    return arr.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+function createTimeOutEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+function hoursWorkedOnDate(date) {
+    let timeIn = this.timeInEvents.find(e => e.date === date)
+    let timeOut = this.timeOutEvents.find(e => e.date === date)
+    return (timeOut.hour - timeIn.hour) / 100
+}
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+function findEmployeeByFirstName(arr, firstName) {
+    return arr.find(e => e.firstName === firstName)
+}
+function calculatePayroll(arr) {
+    return arr.reduce((total, e) => total + allWagesFor.call(e), 0)
+}
